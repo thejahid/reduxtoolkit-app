e9a4b35e7536313faaee7b9ce0eb8189d1ba5eda
developerjahid <jhpabel@gmail.com>
2021-07-27 19:13:28 +0600

    app js completed

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Redux Modern App with Toolkit!</p>
+        <div className="main">
+          <button type="button">+</button>
+          Number
+          <button type="button">-</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
